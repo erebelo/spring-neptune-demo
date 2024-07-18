deeb4fbb4903aa7ec16dc46893732b6b00ee2370
erebelo <eduldrebelo@gmail.com>
2024-07-18 20:53:37 -0300

    Adjusting findAll unit test

--- src/test/java/com/erebelo/springneptunedemo/repository/UserRepositoryTest.java
+++ src/test/java/com/erebelo/springneptunedemo/repository/UserRepositoryTest.java
@@ -2,6 +2,7 @@ package com.erebelo.springneptunedemo.repository;
 
 import com.erebelo.springneptunedemo.domain.graph.node.UserNode;
 import com.erebelo.springneptunedemo.repository.impl.UserRepositoryImpl;
+import org.apache.tinkerpop.gremlin.process.traversal.P;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversal;
 import org.apache.tinkerpop.gremlin.process.traversal.dsl.graph.GraphTraversalSource;
 import org.apache.tinkerpop.gremlin.structure.Vertex;
@@ -15,7 +16,12 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.erebelo.springneptunedemo.util.QueryUtil.propertyLikeRegex;
+import static com.erebelo.springneptunedemo.util.QueryUtil.propertyRegex;
+import static org.apache.tinkerpop.gremlin.process.traversal.TextP.regex;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.anyString;
 import static org.mockito.Mockito.verify;
@@ -35,25 +41,32 @@ class UserRepositoryTest {
     @Mock
     private GraphTraversal<Vertex, Map<Object, Object>> gtVertexMap;
 
-//    @Test
-//    void testFindAllSuccessfully() {
-//        Map<Object, Object> map = new HashMap<>();
-//        map.put("id", "123");
-//
-//        given(traversalSource.V()).willReturn(gtVertex);
-//        given(gtVertex.hasLabel(anyString())).willReturn(gtVertex);
-//        given(gtVertex.elementMap()).willReturn(gtVertexMap);
-//        given(gtVertexMap.toList()).willReturn(Collections.singletonList(map));
-//
-//        var result = repository.findAll();
-//
-//        assertThat(result).isNotNull();
-//
-//        verify(traversalSource).V();
-//        verify(gtVertex).hasLabel("User");
-//        verify(gtVertex).elementMap();
-//        verify(gtVertexMap).toList();
-//    }
+    @Test
+    void testFindAllSuccessfully() {
+        Map<Object, Object> map = new HashMap<>();
+        map.put("id", "123");
+
+        given(traversalSource.V()).willReturn(gtVertex);
+        given(gtVertex.hasLabel(anyString())).willReturn(gtVertex);
+        given(gtVertex.has(anyString(), any(P.class))).willReturn(gtVertex);
+        given(gtVertex.range(anyLong(), anyLong())).willReturn(gtVertex);
+        given(gtVertex.elementMap()).willReturn(gtVertexMap);
+        given(gtVertexMap.toList()).willReturn(Collections.singletonList(map));
+
+        var result = repository.findAll("John", "CA", 10, 1);
+
+        assertThat(result).isNotNull();
+        assertThat(result).hasSize(1);
+        assertThat(result.get(0).getId()).isEqualTo("123");
+
+        verify(traversalSource).V();
+        verify(gtVertex).hasLabel("User");
+        verify(gtVertex).has("name", regex("(?i)" + propertyLikeRegex("John")));
+        verify(gtVertex).has("address_state", regex("(?i)" + propertyRegex("CA")));
+        verify(gtVertex).range(0, 10);
+        verify(gtVertex).elementMap();
+        verify(gtVertexMap).toList();
+    }
 
     @Test
     void testInsertSuccessfully() {
